d74970f94eb93e9054b7aeb05c5caa6a781b137a
trainliveapp <Trainliveapp@gmail.com>
2026-07-31 00:39:31 +0100

    Remove double router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomeDashboard from './pages/HomeDashboard'
 import SignInPage from './pages/SignInPage'
 import SignupPage from './pages/SignupPage'
@@ -54,16 +54,14 @@ export default function App() {
   }
 
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<HomeDashboard />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/check-email" element={<CheckEmailPage />} />
-        <Route path="/reset-password" element={<CreatePasswordPage />} />
-        <Route path="/alerts" element={<AlertsPage />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<HomeDashboard />} />
+      <Route path="/signin" element={<SignInPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/check-email" element={<CheckEmailPage />} />
+      <Route path="/reset-password" element={<CreatePasswordPage />} />
+      <Route path="/alerts" element={<AlertsPage />} />
+    </Routes>
   )
 }
