eb4bd2cd58d545edb5da7248d2df5aed79150ec2
trainliveapp <Trainliveapp@gmail.com>
2026-07-31 00:27:41 +0100

    Fix routing for GitHub pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import HomeDashboard from './pages/HomeDashboard'
 import SignInPage from './pages/SignInPage'
 import SignupPage from './pages/SignupPage'
@@ -39,13 +39,8 @@ export default function App() {
     }
   }, [showSplash])
 
-  if (showSplash) {
-    return <SplashScreen fadingOut={fadingOut} />
-  }
-
-  if (showPromo) {
-    return <PretPromoScreen fadingOut={promoFadingOut} />
-  }
+  if (showSplash) return <SplashScreen fadingOut={fadingOut} />
+  if (showPromo) return <PretPromoScreen fadingOut={promoFadingOut} />
 
   if (showWelcome) {
     return (
@@ -59,14 +54,16 @@ export default function App() {
   }
 
   return (
-    <Routes>
-      <Route path="/" element={<HomeDashboard />} />
-      <Route path="/signin" element={<SignInPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-      <Route path="/check-email" element={<CheckEmailPage />} />
-      <Route path="/reset-password" element={<CreatePasswordPage />} />
-      <Route path="/alerts" element={<AlertsPage />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomeDashboard />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/check-email" element={<CheckEmailPage />} />
+        <Route path="/reset-password" element={<CreatePasswordPage />} />
+        <Route path="/alerts" element={<AlertsPage />} />
+      </Routes>
+    </HashRouter>
   )
 }
